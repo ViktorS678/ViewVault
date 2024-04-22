a9123d8466ed88289837e88b24b1fe5c0c57a57a
Viktor678 <viktor678_1998@abv.bg>
2024-04-22 08:57:28 +0300

    basic DataConstants added

--- ViewVault.Infrastructure/Constants/DataConstants.cs
+++ ViewVault.Infrastructure/Constants/DataConstants.cs
@@ -8,6 +8,20 @@ namespace ViewVault.Infrastructure.Constants
 {
     public static class DataConstants
     {
+        //Movies' and Actors' constraints
+
+        public const int NamesMinLength = 3;
+        public const int NamesMaxLength = 50;
+
+        //Descriptions', Autobiographies' and Comments' constraints
+
+        public const int DescriptionsMinLength = 1;
+        public const int DescriptionsMaxLength = 300;
+
+        //Others
+
+        public const string DateOnlyFormat = "yyyy-MM-dd";
+        public const string DateTimeFormat = "yyyy-MM-dd HH:mm:ss";
 
     }
 }
--- ViewVault.Infrastructure/Data/Models/Core/Genre.cs
+++ ViewVault.Infrastructure/Data/Models/Core/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace ViewVault.Infrastructure.Data.Models.Core
     public class Genre
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
 
         public virtual ICollection<MovieGenre> MovieGenres { get; set; }
