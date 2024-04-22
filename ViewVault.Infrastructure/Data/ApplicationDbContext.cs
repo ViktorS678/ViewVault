5e9327b05879120d157f4da1a62bca775ea195e7
Viktor678 <viktor678_1998@abv.bg>
2024-04-22 11:16:45 +0300

    DbContext initial

--- ViewVault.Infrastructure/Data/ApplicationDbContext.cs
+++ ViewVault.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+using ViewVault.Infrastructure.Data.Models.Core;
+using ViewVault.Infrastructure.Data.Models.Linked;
 
 namespace ViewVault.Infrastructure.Data
 {
@@ -9,5 +12,44 @@ namespace ViewVault.Infrastructure.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<MovieActor>()
+                 .HasKey(x => new { x.MovieId, x.ActorId });
+
+            builder.Entity<MovieLanguage>()
+                 .HasKey(x => new { x.MovieId, x.LanguageId });
+
+            builder.Entity<MovieGenre>()
+                 .HasKey(x => new { x.MovieId, x.GenreId });
+
+            builder.Entity<Rating>()
+               .HasKey(x => new { x.MovieId, x.UserId });
+
+
+            base.OnModelCreating(builder);
+
+        }
+        
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Actor> Actors { get; set; }
+
+        public DbSet<MovieActor> MoviesActors { get; set; }
+
+        public DbSet<Language> Languages { get; set; }
+
+        public DbSet<MovieLanguage> MoviesLanguages { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<MovieGenre> MoviesGenres { get; set; }
+
+        public DbSet<MovieComment> MovieComments { get; set; }
+
+        public DbSet<Rating> Ratings { get; set; }
+
+
     }
 }
