e794112984de202581af48446baf3647b5d1e60f
Viktor678 <viktor678_1998@abv.bg>
2024-04-22 10:52:18 +0300

    DataConstants annoying using

--- ViewVault.Infrastructure/Constants/DataConstants.cs
+++ ViewVault.Infrastructure/Constants/DataConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ViewVault.Infrastructure.Constants
+﻿namespace ViewVault.Infrastructure.Constants
 {
     public static class DataConstants
     {
