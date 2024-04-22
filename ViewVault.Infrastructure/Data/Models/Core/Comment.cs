27e69b09c53fd22576e1919c1b4bc8a551a5e0bc
Viktor678 <viktor678_1998@abv.bg>
2024-04-22 05:47:01 +0300

    Movie comments and rating basic set-up

--- ViewVault.Infrastructure/Data/Models/Core/Comment.cs
+++ ViewVault.Infrastructure/Data/Models/Core/Comment.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewVault.Infrastructure.Data.Models.Linked;
 
 namespace ViewVault.Infrastructure.Data.Models.Core
 {
-    internal class Comment
+    public class Comment
     {
+        [Required]
+        public string Content { get; set; }
+
+        public virtual ICollection<MovieComment> MovieComments { get; set; }
     }
 }
--- ViewVault.Infrastructure/Data/Models/Linked/Rating.cs
+++ ViewVault.Infrastructure/Data/Models/Linked/Rating.cs
@@ -1,6 +1,16 @@
-﻿namespace ViewVault.Infrastructure.Data.Models.Linked
+﻿using System.ComponentModel.DataAnnotations;
+using ViewVault.Infrastructure.Data.Models.Core;
+
+namespace ViewVault.Infrastructure.Data.Models.Linked
 {
     public class Rating
     {
+        public int MovieId { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        public double RatingValue { get; set; }
+
+        public virtual Movie Movie { get; set; }
+   //     public virtual ApplicationUser User { get; set; }
     }
 }
