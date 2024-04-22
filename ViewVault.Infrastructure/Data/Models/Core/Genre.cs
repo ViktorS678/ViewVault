e28f8f1a7153e80618619315c46cc77f48d4af84
Viktor678 <viktor678_1998@abv.bg>
2024-04-22 04:26:41 +0300

    Movie genres basic set-up

--- ViewVault.Infrastructure/Data/Models/Core/Genre.cs
+++ ViewVault.Infrastructure/Data/Models/Core/Genre.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewVault.Infrastructure.Data.Models.Linked;
 
 namespace ViewVault.Infrastructure.Data.Models.Core
 {
-    internal class Genre
+    public class Genre
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<MovieGenre> MovieGenres { get; set; }
     }
 }
--- ViewVault.Infrastructure/Data/Models/Linked/MovieGenre.cs
+++ ViewVault.Infrastructure/Data/Models/Linked/MovieGenre.cs
@@ -1,6 +1,22 @@
-﻿namespace ViewVault.Infrastructure.Data.Models.Linked
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using ViewVault.Infrastructure.Data.Models.Core;
+
+namespace ViewVault.Infrastructure.Data.Models.Linked
 {
     public class MovieGenre
     {
+        [Required]
+        public int GenreId { get; set; }
+
+        [ForeignKey(nameof(GenreId))]
+        public Genre Genre { get; set; } = null!;
+
+        [Required]
+        public int MovieId { get; set; }
+
+        [ForeignKey(nameof(MovieId))]
+        public virtual Movie Movie { get; set; }
     }
 }
