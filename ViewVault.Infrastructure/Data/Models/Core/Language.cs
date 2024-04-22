361a000ddbdf5f3841c806486fe3d4ca65d981a3
Viktor678 <viktor678_1998@abv.bg>
2024-04-22 04:40:41 +0300

    Movie languages basic set-up

--- ViewVault.Infrastructure/Data/Models/Core/Language.cs
+++ ViewVault.Infrastructure/Data/Models/Core/Language.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewVault.Infrastructure.Data.Models.Linked;
 
 namespace ViewVault.Infrastructure.Data.Models.Core
 {
-    internal class Language
+    public class Language
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<MovieLanguage> MovieLanguages { get; set; }
     }
 }
--- ViewVault.Infrastructure/Data/Models/Linked/MovieLanguage.cs
+++ ViewVault.Infrastructure/Data/Models/Linked/MovieLanguage.cs
@@ -1,8 +1,21 @@
-﻿namespace ViewVault.Infrastructure.Data.Models.Linked
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ViewVault.Infrastructure.Data.Models.Core;
+
+namespace ViewVault.Infrastructure.Data.Models.Linked
 {
     public class MovieLanguage
     {
+        [Required]
+        public int MovieId { get; set; }
+
+        [ForeignKey(nameof(MovieId))]
+        public Movie Movie { get; set; }
+
+        [Required]
         public int LanguageId { get; set; }
-        public string Name { get; set; }
+
+        [ForeignKey(nameof(LanguageId))]
+        public Language Language { get; set; }
     }
 }
