614a2a0d6f6ce2c1b014def6d779b2295c5b7760
Viktor678 <viktor678_1998@abv.bg>
2024-04-22 09:14:22 +0300

    Parsing RatingValue to match desired outcome

--- ViewVault.Infrastructure/Data/Models/Linked/Rating.cs
+++ ViewVault.Infrastructure/Data/Models/Linked/Rating.cs
@@ -9,7 +9,14 @@ namespace ViewVault.Infrastructure.Data.Models.Linked
         [Required]
         public string UserId { get; set; }
         public double RatingValue { get; set; }
-
+        public string FormattedRatingValue
+        {
+            get
+            {
+                // Convert the double value to a string with the desired format
+                return RatingValue.ToString("0.0");
+            }
+        }
         public virtual Movie Movie { get; set; }
    //     public virtual ApplicationUser User { get; set; }
     }
