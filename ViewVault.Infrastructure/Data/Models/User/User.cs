db8261227b421773ef98467d9f5ca33f4dcd2a06
Viktor678 <viktor678_1998@abv.bg>
2024-04-24 05:48:15 +0300

    Initial users set-up

---
+++ ViewVault.Infrastructure/Data/Models/User/User.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ViewVault.Infrastructure.Data.Common.Moderation;
+using ViewVault.Infrastructure.Data.Models.Linked;
+
+namespace ViewVault.Infrastructure.Data.Models.User
+{
+    public class User : IdentityUser, ICreate, IDelete
+    {
+
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Roles = new HashSet<IdentityUserRole<string>>();
+            this.Claims = new HashSet<IdentityUserClaim<string>>();
+            this.Logins = new HashSet<IdentityUserLogin<string>>();
+
+            this.Ratings = new HashSet<Rating>();
+            this.MovieComments = new HashSet<MovieComment>();
+        }
+
+
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedOn { get; set; }
+
+
+        public virtual ICollection<IdentityUserRole<string>> Roles { get; set; }
+        public virtual ICollection<IdentityUserClaim<string>> Claims { get; set; }
+        public virtual ICollection<IdentityUserLogin<string>> Logins { get; set; }
+        public virtual ICollection<Rating> Ratings { get; set; }
+        public virtual ICollection<MovieComment> MovieComments { get; set; }
+
+    }
+}
---
+++ ViewVault.Infrastructure/Data/Models/User/UserRole.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ViewVault.Infrastructure.Data.Common.Moderation;
+
+namespace ViewVault.Infrastructure.Data.Models.User
+{
+    public class UserRole : IdentityRole, ICreate, IDelete
+    {
+        public UserRole()
+            : this(null)
+        {
+        }
+
+        public UserRole(string name)
+            : base(name)
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
