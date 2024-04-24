cf040f6f148dc5284af5813acbc3d453a93f66c7
Viktor678 <viktor678_1998@abv.bg>
2024-04-24 08:07:57 +0300

    moving lonely config

---
+++ ViewVault.Infrastructure/Data/Context/Extensions/UserConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ViewVault.Infrastructure.Data.Models.User;
+
+namespace ViewVault.Infrastructure.Data.Context.Extensions
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> appUser)
+        {
+            appUser
+                .HasMany(e => e.Claims)
+                .WithOne()
+                .HasForeignKey(e => e.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            appUser
+                .HasMany(e => e.Logins)
+                .WithOne()
+                .HasForeignKey(e => e.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            appUser
+                .HasMany(e => e.Roles)
+                .WithOne()
+                .HasForeignKey(e => e.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
